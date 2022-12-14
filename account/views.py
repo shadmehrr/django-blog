f7998c8a8372b3a3ba145ea4fa7b3e3f3ef296f2
shadmehrr <shadmehrr@outlook.com>
2022-12-15 02:53:34 +0330

    First

---
+++ account/views.py
@@ -0,0 +1,78 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView, PasswordChangeView
+from .models import User
+from .forms import ProfileForm
+from .mixins import (
+	FieldsMixin,
+	FormValidMixin,
+	AuthorAccessMixin,
+	AuthorsAccessMixin,
+	SuperUserAccessMixin
+)
+from django.views.generic import (
+	ListView,
+	CreateView,
+	UpdateView,
+	DeleteView
+)
+from blog.models import Article
+
+# Create your views here.
+class ArticleList(AuthorsAccessMixin, ListView):
+	template_name = "registration/home.html"
+
+	def get_queryset(self):
+		if self.request.user.is_superuser:
+			return Article.objects.all()
+		else:
+			return Article.objects.filter(author=self.request.user)
+
+
+class ArticleCreate(AuthorsAccessMixin, FormValidMixin, FieldsMixin, CreateView):
+	model = Article
+	template_name = "registration/article-create-update.html"
+
+
+class ArticleUpdate(AuthorAccessMixin, FormValidMixin, FieldsMixin, UpdateView):
+	model = Article
+	template_name = "registration/article-create-update.html"
+
+
+class ArticleDelete(SuperUserAccessMixin, DeleteView):
+	model = Article
+	success_url = reverse_lazy('account:home')
+	template_name = "registration/article_confirm_delete.html"
+
+
+class Profile(LoginRequiredMixin ,UpdateView):
+	model = User
+	template_name = "registration/profile.html"
+	form_class = ProfileForm
+	success_url = reverse_lazy("account:profile")
+
+	def get_object(self):
+		return User.objects.get(pk = self.request.user.pk)
+
+	def get_form_kwargs(self):
+		kwargs = super(Profile, self).get_form_kwargs()
+		kwargs.update({
+			'user': self.request.user
+		})
+		return kwargs
+
+
+class Login(LoginView):
+	def get_success_url(self):
+		user = self.request.user
+
+		if user.is_superuser or user.is_author:
+			return reverse_lazy("account:home")
+		else:
+			return reverse_lazy("account:profile")
+
+
+class PasswordChange(PasswordChangeView):
+	success_url = reverse_lazy("account:password_change_done")
---
+++ blog/templatetags/base_tags.py
@@ -0,0 +1,26 @@
+from django import template
+from ..models import Category
+
+register = template.Library()
+
+@register.simple_tag
+def title():
+    return "وبلاگ جنگوی من"
+
+
+@register.inclusion_tag("blog/partials/category_navbar.html")
+def category_navbar():
+	return {
+		"category": Category.objects.filter(status=True)
+	}
+
+
+@register.inclusion_tag("registration/partials/link.html")
+def link(request, link_name, content, classes):
+	return {
+		"request": request,
+		"link_name": link_name,
+		"link": "account:{}".format(link_name),
+		"content": content,
+		"classes": classes,
+	}
